5b4e664953a0e52194e90a38a4b442ea0894f9da
Rohanjai <jairohan2912@gmail.com>
2023-06-25 21:08:18 +0530

    Expenses List Widget Created and Added

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -32,11 +33,13 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Column(
       children: [
-        Text('Expenses Chart'),
-        Text('Expenses'),
+        const Text('Expenses Chart'),
+        Expanded(
+          child: ExpensesList(expenses: _registeredExpenses),
+        ),
       ],
     ));
   }
