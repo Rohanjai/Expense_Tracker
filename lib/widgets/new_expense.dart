adc941d1e956b82c93e5798a67f1219188af4d90
Rohanjai <jairohan2912@gmail.com>
2023-06-26 14:32:49 +0530

    DropDown Menu Configured

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -12,6 +12,8 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.shopping;
+
   void _presentDatePicker() async {
     final initialDate = DateTime.now();
     final firstDate =
@@ -81,7 +83,27 @@ class _NewExpenseState extends State<NewExpense> {
               )
             ],
           ),
+          const SizedBox(height: 16),
           Row(children: [
+            DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name.toUpperCase()),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                }),
+            const Spacer(),
             TextButton(
                 onPressed: () {
                   Navigator.pop(context);
