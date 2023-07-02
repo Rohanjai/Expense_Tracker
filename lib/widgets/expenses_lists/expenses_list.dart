40b3a02ce0eeee1a3c82f72d0d55e10b04228934
Rohanjai <jairohan2912@gmail.com>
2023-07-02 07:57:28 +0530

    Themedata setup

--- lib/widgets/expenses_lists/expenses_list.dart
+++ lib/widgets/expenses_lists/expenses_list.dart
@@ -12,12 +12,17 @@ class ExpensesList extends StatelessWidget {
     return ListView.builder(
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
-        onDismissed: (direction) {
-          onRemoveExpense(expenses[index]);
-        },
         key: ValueKey(
           expenses[index],
         ),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.85),
+          margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+        ),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
         child: ExpenseItem(expense: expenses[index]),
       ),
     );
